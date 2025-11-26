2d11765211b5845bbbf0560f891c191cabc5e453
Jemimah Jemutai <jemutaijemimah@gmail.com>
2025-11-27 01:26:07 +0300

    Create urls.py

---
+++ accounts/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    # Authentication
+    path('login/', views.PulseLoginView.as_view(), name='login'),
+    path('logout/', views.PulseLogoutView.as_view(), name='logout'),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+    
+    # Onboarding wizard
+    path('onboarding/gender/', views.OnboardingGenderView.as_view(), name='onboarding_gender'),
+    path('onboarding/age/', views.OnboardingAgeView.as_view(), name='onboarding_age'),
+    path('onboarding/metrics/', views.OnboardingMetricsView.as_view(), name='onboarding_metrics'),
+    path('onboarding/goal/', views.OnboardingGoalView.as_view(), name='onboarding_goal'),
+    
+    # Profile
+    path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('profile/edit/', views.ProfileEditView.as_view(), name='profile_edit'),
+]
